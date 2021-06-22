3b92ac6dc581a5ad7ac4345f4f5eb37751b47779
Andres Ladino <aladinoster@gmail.com>
2021-06-22 16:50:21 +0200

    👨🏻‍💻 Adjustments comments

--- 1-variables/main.cpp
+++ 1-variables/main.cpp
@@ -2,17 +2,17 @@
 
 int main()
 {
-    int variable = 8;            // -2b / 2b
-    unsigned int variable2 = 10; // 0 / 4b
-    char a = 65;                 // 1byte
+    int variable = 8;            // 4 bytes
+    unsigned int variable2 = 10; // 4 bytes
+    char a = 65;                 // 1 byte
     char car = 'a';              //
-    short b = 2;                 // 2bytes
-    int c = 4;                   // 4bytes
-    long d = 5;                  // 4bytes (complier conditioned)
-    long int e = 6;              // 8bytes
-    float floating = 5.5f;       // 4bytes
-    double var = 5.2;            // 8bytes
-    bool decide = true;          //  1 byte
+    short b = 2;                 // 2 bytes
+    int c = 4;                   // 4 bytes
+    long d = 5;                  // 4 bytes (complier conditioned)
+    long int e = 6;              // 8 bytes
+    float floating = 5.5f;       // 4 bytes
+    double var = 5.2;            // 8 bytes
+    bool decide = true;          // 1 byte
 
     std::cout << variable << std::endl;
 
