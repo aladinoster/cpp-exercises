2d5e8079f055a73947d986120802672378f1c24c
Andres Ladino <aladinoster@gmail.com>
2021-06-22 09:28:09 +0200

    👨🏻‍💻 Static Pt II

--- 15-static/main.cpp
+++ 15-static/main.cpp
@@ -8,6 +8,10 @@ struct Entity
     {
         std::cout << X << " , " << Y << std::endl;
     }
+    static void Printer()
+    {
+        std::cout << X << " , " << Y << std::endl; //static methods cannot access not static things
+    }
 };
 
 int Entity::X; // Single instance of this variable across all instances of the struct variables
@@ -18,6 +22,8 @@ int main()
     Entity e1;
     e1.X = 2;
     e1.Y = 3;
+    Entity::X = 2;
+    Entity::Y = 3;
 
     // Entity e2 = {5, 8};  Alternative initializer
 
@@ -26,9 +32,13 @@ int main()
     Entity e2;
     e2.X = 5;
     e2.Y = 8;
+    Entity::X = 5;
+    Entity::Y = 8;
 
     e1.Print();
     e2.Print();
+
+    Entity::Printer();
 
     std::cin.get();
 }
